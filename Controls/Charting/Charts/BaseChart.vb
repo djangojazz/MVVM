d0994fce42198fbec966d8ab9d9fcbed82194217
Brett Morin <brettmorin2@gmail.com>
2017-02-10 13:07:21 -0800

    Made changes to hook up hidden properties

--- Controls/Charting/Charts/BaseChart.vb
+++ Controls/Charting/Charts/BaseChart.vb
@@ -1,11 +1,28 @@
-﻿Public Class BaseChart
+﻿Imports System.Windows.Threading
+
+Public Class BaseChart
   Inherits UserControl
+  Private _xType As Type
+  Private _xFloor As Decimal = 0
+  Private _xCeiling As Decimal = 0
+  Private _yType As Type
+  Private _yFloor As Decimal = 0
+  Private _yCeiling As Decimal = 0
+  Private _viewHeight As Double = 0
+  Private _viewWidth As Double = 0
+  Private _tickWidth As Double = 0
+  Private _tickHeight As Double = 0
+  Private _labelWidth As Double = 0
+  Private _labelHeight As Double = 0
+  Private _timer As New DispatcherTimer
+  Private _defaultTimeSpan As New TimeSpan(1000)
+
 
 #Region "Dependent Properties"
 
 
 #Region "ChartTitle"
-  Public Shared ReadOnly ChartTitleProperty As DependencyProperty = DependencyProperty.Register("ChartTitle", GetType(String), GetType(LineChart), New UIPropertyMetadata(String.Empty))
+  Public Shared ReadOnly ChartTitleProperty As DependencyProperty = DependencyProperty.Register(NameOf(ChartTitle), GetType(String), GetType(LineChart), New UIPropertyMetadata(String.Empty))
 
   Public Property ChartTitle As String
     Get
@@ -18,7 +35,7 @@
 #End Region
 
 #Region "ChartForeground"
-  Public Shared ReadOnly ChartForegroundProperty As DependencyProperty = DependencyProperty.Register("ChartForeground", GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
+  Public Shared ReadOnly ChartForegroundProperty As DependencyProperty = DependencyProperty.Register(NameOf(ChartForeground), GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
 
   Public Property ChartForeground As Brush
     Get
@@ -30,8 +47,22 @@
   End Property
 #End Region
 
+#Region "ChartTitleVisibility"
+  Public Shared ReadOnly ChartTitleVisibilityProperty As DependencyProperty = DependencyProperty.Register(NameOf(ChartTitleVisibility), GetType(Visibility), GetType(BaseChart), New PropertyMetadata(Visibility.Visible))
+
+  Public Property ChartTitleVisibility As Visibility
+    Get
+      Return GetValue(ChartTitleVisibilityProperty)
+    End Get
+
+    Set(ByVal value As Visibility)
+      SetValue(ChartTitleVisibilityProperty, value)
+    End Set
+  End Property
+#End Region
+
 #Region "ChartTitleHidden"
-  Public Shared ReadOnly ChartTitleHiddenProperty As DependencyProperty = DependencyProperty.Register("ChartTitleHidden", GetType(Boolean), GetType(LineChart), New UIPropertyMetadata(False, AddressOf LineChart.ChartTitleHiddenChanged))
+  Public Shared ReadOnly ChartTitleHiddenProperty As DependencyProperty = DependencyProperty.Register(NameOf(ChartTitleHidden), GetType(Boolean), GetType(LineChart), New UIPropertyMetadata(False, AddressOf LineChart.ChartTitleHiddenChanged))
 
   Public Property ChartTitleHidden As Boolean
     Get
@@ -43,19 +74,19 @@
   End Property
 
   Public Shared Sub ChartTitleHiddenChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-    Dim LC As LineChart = DirectCast(d, LineChart)
+    Dim o = DirectCast(d, BaseChart)
 
     If CBool(e.NewValue) Then
-      LC.PART_ChartTitle.Visibility = Visibility.Collapsed
+      o.ChartTitleVisibility = Visibility.Collapsed
     Else
-      LC.PART_ChartTitle.Visibility = Visibility.Visible
+      o.ChartTitleVisibility = Visibility.Visible
     End If
 
   End Sub
 #End Region
 
 #Region "BackGroundColor"
-  Public Shared ReadOnly BackGroundColorProperty As DependencyProperty = DependencyProperty.Register("BackGroundColor", GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
+  Public Shared ReadOnly BackGroundColorProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackGroundColor), GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
 
   Public Property BackGroundColor As Brush
     Get
@@ -68,7 +99,7 @@
 #End Region
 
 #Region "BackGroundColorCanvas"
-  Public Shared ReadOnly BackGroundColorCanvasProperty As DependencyProperty = DependencyProperty.Register("BackGroundColorCanvas", GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
+  Public Shared ReadOnly BackGroundColorCanvasProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackGroundColorCanvas), GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
 
   Public Property BackGroundColorCanvas As Brush
     Get
@@ -81,7 +112,7 @@
 #End Region
 
 #Region "BackGroundColorLegend"
-  Public Shared ReadOnly BackGroundColorLegendProperty As DependencyProperty = DependencyProperty.Register("BackGroundColorLegend", GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
+  Public Shared ReadOnly BackGroundColorLegendProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackGroundColorLegend), GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
 
   Public Property BackGroundColorLegend As Brush
     Get
@@ -93,10 +124,36 @@
   End Property
 #End Region
 
+#Region "LegendVisibility"
+  Public Shared ReadOnly LegendVisibilityProperty As DependencyProperty = DependencyProperty.Register(NameOf(LegendVisibility), GetType(Visibility), GetType(BaseChart), New PropertyMetadata(Visibility.Visible))
 
+  Public Property LegendVisibility As Visibility
+    Get
+      Return GetValue(LegendVisibilityProperty)
+    End Get
+
+    Set(ByVal value As Visibility)
+      SetValue(LegendVisibilityProperty, value)
+    End Set
+  End Property
+#End Region
+
+#Region "LegendTextVisibility"
+  Public Shared ReadOnly LegendTextVisibilityProperty As DependencyProperty = DependencyProperty.Register(NameOf(LegendTextVisibility), GetType(Visibility), GetType(BaseChart), New PropertyMetadata(Visibility.Visible))
+
+  Public Property LegendTextVisibility As Visibility
+    Get
+      Return GetValue(LegendTextVisibilityProperty)
+    End Get
+
+    Set(ByVal value As Visibility)
+      SetValue(LegendTextVisibilityProperty, value)
+    End Set
+  End Property
+#End Region
 
 #Region "LegendForeground"
-  Public Shared ReadOnly LegendForegroundProperty As DependencyProperty = DependencyProperty.Register("LegendForeground", GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black, AddressOf LineChart.ChartLegendTextChanged))
+  Public Shared ReadOnly LegendForegroundProperty As DependencyProperty = DependencyProperty.Register(NameOf(LegendForeground), GetType(Brush), GetType(LineChart), New UIPropertyMetadata(Brushes.Black))
 
   Public Property LegendForeground As Brush
     Get
@@ -107,14 +164,10 @@
     End Set
   End Property
 
-  Public Shared Sub ChartLegendTextChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-    Dim LC As LineChart = DirectCast(d, LineChart)
-    LC.PART_LEGENDTEXT.Visibility = Visibility.Visible
-  End Sub
 #End Region
 
 #Region "LegendHidden"
-  Public Shared ReadOnly LegendHiddenProperty As DependencyProperty = DependencyProperty.Register("LegendHidden", GetType(Boolean), GetType(LineChart), New UIPropertyMetadata(False, AddressOf LineChart.ChartLegendHiddenChanged))
+  Public Shared ReadOnly LegendHiddenProperty As DependencyProperty = DependencyProperty.Register(NameOf(LegendHidden), GetType(Boolean), GetType(LineChart), New UIPropertyMetadata(False, AddressOf LineChart.ChartLegendHiddenChanged))
 
   Public Property LegendHidden As Boolean
     Get
@@ -126,23 +179,20 @@
   End Property
 
   Public Shared Sub ChartLegendHiddenChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-    Dim LC As LineChart = DirectCast(d, LineChart)
+    Dim o = DirectCast(d, BaseChart)
 
     If CBool(e.NewValue) Then
-      LC.PART_LEGEND.Visibility = Visibility.Collapsed
-      LC.PART_LEGENDTEXT.Visibility = Visibility.Collapsed
+      o.LegendVisibility = Visibility.Collapsed
+      o.LegendTextVisibility = Visibility.Collapsed
     Else
-      LC.PART_LEGENDTEXT.Visibility = Visibility.Visible
-      LC.PART_LEGEND.Visibility = Visibility.Visible
+      o.LegendVisibility = Visibility.Visible
+      o.LegendTextVisibility = Visibility.Visible
     End If
-
   End Sub
 #End Region
 
-
-
 #Region "NumberOfTicks"
-  Public Shared ReadOnly NumberOfTicksProperty As DependencyProperty = DependencyProperty.Register("NumberOfTicks", GetType(Integer), GetType(LineChart), New UIPropertyMetadata(0))
+  Public Shared ReadOnly NumberOfTicksProperty As DependencyProperty = DependencyProperty.Register(NameOf(NumberOfTicks), GetType(Integer), GetType(LineChart), New UIPropertyMetadata(0))
 
   Public Property NumberOfTicks As Integer
     Get
@@ -155,7 +205,7 @@
 #End Region
 
 #Region "XValueConverter"
-  Public Shared ReadOnly XValueConverterProperty As DependencyProperty = DependencyProperty.Register("XValueConverter", GetType(IValueConverter), GetType(LineChart), Nothing)
+  Public Shared ReadOnly XValueConverterProperty As DependencyProperty = DependencyProperty.Register(NameOf(XValueConverter), GetType(IValueConverter), GetType(LineChart), Nothing)
 
   Public Property XValueConverter As IValueConverter
     Get
@@ -168,7 +218,7 @@
 #End Region
 
 #Region "YValueConverter"
-  Public Shared ReadOnly YValueConverterProperty As DependencyProperty = DependencyProperty.Register("YValueConverter", GetType(IValueConverter), GetType(LineChart), Nothing)
+  Public Shared ReadOnly YValueConverterProperty As DependencyProperty = DependencyProperty.Register(NameOf(YValueConverter), GetType(IValueConverter), GetType(LineChart), Nothing)
 
   Public Property YValueConverter As IValueConverter
     Get
@@ -181,7 +231,7 @@
 #End Region
 
 #Region "FontType"
-  Public Shared ReadOnly FontTypeProperty As DependencyProperty = DependencyProperty.Register("FontType", GetType(FontFamily), GetType(LineChart), Nothing)
+  Public Shared ReadOnly FontTypeProperty As DependencyProperty = DependencyProperty.Register(NameOf(FontType), GetType(FontFamily), GetType(LineChart), Nothing)
 
   Public Property FontType As FontFamily
     Get
---
+++ Controls/Charting/IChart.vb
@@ -0,0 +1,3 @@
+﻿Public Interface IChart
+
+End Interface

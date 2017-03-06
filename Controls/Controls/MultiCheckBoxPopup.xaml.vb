d2c366934fb1b01c02e8382c05ec5bee4c7b0444
Brett Morin <brettmorin2@gmail.com>
2017-03-06 09:16:20 -0800

    Simplified

--- Controls/Controls/MultiCheckBoxPopup.xaml.vb
+++ Controls/Controls/MultiCheckBoxPopup.xaml.vb
@@ -50,56 +50,5 @@ Public Class MultiCheckBoxPopup
     End Set
   End Property
 #End Region
-  'Public Shared ReadOnly ItemsCollectionProperty As DependencyProperty = DependencyProperty.Register("ItemsCollection", GetType(ObservableCollection(Of Object)), GetType(MultiCheckBoxPopup), New UIPropertyMetadata(Nothing, AddressOf ItemCollectionChanged))
-
-  'Private Shared Sub ItemCollectionChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-  '  Dim o = DirectCast(d, MultiCheckBoxPopup)
-
-  '  If Not IsNothing(e.OldValue) Then
-  '    Dim OldCollection = TryCast(e.OldValue, ObservableCollection(Of Object))
-  '    RemoveHandler OldCollection.CollectionChanged, AddressOf o.CollectionChanged
-  '  End If
-
-  '  If Not IsNothing(e.NewValue) Then
-  '    Dim NewCollection = TryCast(e.NewValue, ObservableCollection(Of Object))
-  '    AddHandler NewCollection.CollectionChanged, AddressOf o.CollectionChanged
-  '  End If
-  'End Sub
-
-  'Private Sub CollectionChanged()
-  '  ItemsCollection = New ObservableCollection(Of Object)
-  'End Sub
-
-  'Public Property ItemsCollection As ObservableCollection(Of Object)
-  '  Get
-  '    Return GetValue(ItemsCollectionProperty)
-  '  End Get
-
-  '  Set(ByVal value As ObservableCollection(Of Object))
-  '    SetValue(ItemsCollectionProperty, value)
-  '  End Set
-  'End Property
-
-  Public Shared ReadOnly PlacementProperty As DependencyProperty = DependencyProperty.Register("Placement", GetType(PlacementMode), GetType(MultiCheckBoxPopup), New UIPropertyMetadata(PlacementMode.Bottom))
-
-  Public Property Placement As PlacementMode
-    Get
-      Return CType(GetValue(PlacementProperty), PlacementMode)
-    End Get
-    Private Set(value As PlacementMode)
-      SetValue(PlacementProperty, value)
-    End Set
-  End Property
-
-  Public ReadOnly Property CommandUpdateHeader As New DelegateCommand(Of Object)(AddressOf CommandUpdateHeaderExecute)
-
-  Private Sub CommandUpdateHeaderExecute(obj As Object)
-    'Dim item To examine = 
-
-    'Dim itemsSelected = ItemsCollection.Where(Function(x) x.Value = True).Select(Function(x) x.Key)
-    'Dim headerUpdated = If(itemsSelected.Any, String.Concat(", ", itemsSelected), "No Items")
-
-    'Header = headerUpdated
-  End Sub
 End Class
 '
